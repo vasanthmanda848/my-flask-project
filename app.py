6ce420b4c90abee573d5f4d4d78ed68406ef53fd
vasanthmanda848 <vasanthmanda843@gmail.com>
2026-08-24 14:48:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,15 +4,16 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return || || ||
-    <html> 
+    return """
+    <html>
     <head>
+        <title>My Flask App</title>
+    </head>
     <body>
-     <h1>Hello Vasikar</h1>
-     </body>
-     </head>
-     </html>|| || ||
-    
+        <h1>Hello Vasikar</h1>
+    </body>
+    </html>
+    """
 
 if __name__ == "__main__":
     app.run(debug=True)
