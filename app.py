268cfcbda589fe4ba07c47eb7b71aea6c0a1472c
vasanthmanda848 <vasanthmanda843@gmail.com>
2026-08-24 14:55:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,14 +7,53 @@ students = ["Vasikar", "Ravi", "Rahul", "Arun"]
 @app.route("/")
 def home():
 
-    result = "<h1>Students</h1><ul>"
+    html = """
+    <html>
+    <head>
+        <title>Student List</title>
+    </head>
+
+    <body style="background-color: lightblue; font-family: Arial;">
+
+        <div style="
+            width: 400px;
+            margin: 100px auto;
+            padding: 30px;
+            background-color: white;
+            text-align: center;
+            border-radius: 10px;
+        ">
+
+            <h1 style="color: darkblue;">
+                Student List
+            </h1>
+
+            <ul style="list-style-type: none; padding: 0;">
+    """
 
     for student in students:
-        result += f"<li>{student}</li>"
+        html += f"""
+                <li style="
+                    background-color: lightgray;
+                    margin: 10px;
+                    padding: 10px;
+                    border-radius: 5px;
+                ">
+                    {student}
+                </li>
+        """
 
-    result += "</ul>"
+    html += """
+            </ul>
 
-    return result
+        </div>
+
+    </body>
+    </html>
+    """
+
+    return html
+
 
 if __name__ == "__main__":
     app.run(debug=True)
