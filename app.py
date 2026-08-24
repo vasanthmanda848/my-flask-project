131a40ee789c9f2d6f9d4467c39aed8eb4fdd447
vasanthmanda848 <vasanthmanda843@gmail.com>
2026-08-24 14:45:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Flask application running success"
+    return || || ||
+    <html> 
+    <head>
+    <body>
+     <h1>Hello Vasikar</h1>
+     </body>
+     </head>
+     </html>|| || ||
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
